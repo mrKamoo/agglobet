fa91280866437e938ca1e2c7b2e9303d4789326c
jeremy bayse <jeremy.bayse@gmail.com>
2026-01-25 18:28:50 +0100

    feat: Add initial dashboard view displaying user statistics, next match details, and recent predictions.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,7 +2,7 @@
     <x-slot name="header">
         <div class="flex justify-between items-center">
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                {{ __('Tableau de bord') }} qui dechire !!!
+                {{ __('Tableau de bord') }}
             </h2>
         </div>
     </x-slot>
