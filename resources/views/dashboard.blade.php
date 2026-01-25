f61aca5b39b95228facf00f569d4d1f72d36a9cf
jeremy bayse <jeremy.bayse@gmail.com>
2026-01-25 18:25:34 +0100

    tableau de bord

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,7 +2,7 @@
     <x-slot name="header">
         <div class="flex justify-between items-center">
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                {{ __('Tableau de bord') }}
+                {{ __('Tableau de bord') }} qui dechire !!!
             </h2>
         </div>
     </x-slot>
