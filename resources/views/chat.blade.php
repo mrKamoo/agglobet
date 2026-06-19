62c3788b0d6170282fc1209962c4cb1b6543e83e
jeremy bayse <jeremy.bayse@gmail.com>
2026-06-19 19:50:50 +0200

    style: augmenter la largeur du conteneur de chat à max-w-7xl sur la page dédiée

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -6,7 +6,7 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div id="app">
                 <dashboard-chat :current-user-id="{{ auth()->id() }}" :current-user-is-admin="{{ auth()->user()->is_admin ? 'true' : 'false' }}" height="h-[650px]"></dashboard-chat>
             </div>
