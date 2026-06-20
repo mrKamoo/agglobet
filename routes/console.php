adba00405204e7877dd1e0ccca0db733029b7e2e
jeremy bayse <jeremy.bayse@gmail.com>
2026-06-21 00:17:30 +0200

    feat: integrate Resend mail driver and add test mail artisan command

--- routes/console.php
+++ routes/console.php
@@ -8,5 +8,18 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
+Artisan::command('mail:test {email}', function () {
+    $email = $this->argument('email');
+    $this->info("Sending test mail to $email...");
+    try {
+        Illuminate\Support\Facades\Mail::raw('Ceci est un mail de test envoye via Resend.', function ($message) use ($email) {
+            $message->to($email)
+                    ->subject('Test Resend Agglobet');
+        });
+        $this->info('Email envoye avec succes !');
+    } catch (\Exception $e) {
+        $this->error("Erreur lors de l'envoi : " . $e->getMessage());
+    }
+})->purpose('Send a test email using Resend');
 
 Schedule::command('football:sync-matches')->hourly();
