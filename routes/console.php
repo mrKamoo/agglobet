bc2321f5cb2121f56d8013e2e4df6e310f84cc52
jeremy bayse <jeremy.bayse@gmail.com>
2026-06-21 00:35:24 +0200

    translation: localize default Laravel mail notifications to French

--- routes/console.php
+++ routes/console.php
@@ -22,4 +22,19 @@ Artisan::command('mail:test {email}', function () {
     }
 })->purpose('Send a test email using Resend');
 
+Artisan::command('mail:test-reset {email}', function () {
+    $email = $this->argument('email');
+    $this->info("Sending test reset password notification to $email...");
+    try {
+        $user = \App\Models\User::firstOrNew(['email' => $email], [
+            'name' => 'Test User',
+            'password' => bcrypt('password'),
+        ]);
+        $user->notify(new \Illuminate\Auth\Notifications\ResetPassword('test-token-123456'));
+        $this->info('Email de reinitialisation envoye avec succes !');
+    } catch (\Exception $e) {
+        $this->error("Erreur lors de l'envoi : " . $e->getMessage());
+    }
+})->purpose('Send a test reset password notification using Resend');
+
 Schedule::command('football:sync-matches')->hourly();
